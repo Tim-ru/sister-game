c32138250e3ca91a2ea14e4fb4dedb0a93012ce9
Tim-ru <httimoha176@gmail.com>
2023-07-25 17:54:15 +0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 10;
+    public float gravityModifier;
+    private Rigidbody playerRb;
+    public bool isOnGround = true;
+    public bool isGameOver;
+    public Animator playerAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        playerAnim= GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !isGameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            isGameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+        }
+    }
+}
